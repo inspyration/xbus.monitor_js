fdb2d44e950a97f55909ef417582583d450d8104
Houzefa Abbasbhay <houzefa.abba@xcg-consulting.fr>
2014-11-26 12:42:20 +0100

    Add input descriptors

---
+++ sources/static/js/collections/input_descriptor.js
@@ -0,0 +1,11 @@
+Models.input_descriptor = Backbone.Model.extend({
+    urlRoot: API_PREFIX + 'input_descriptor'
+});
+
+Collections.input_descriptor = Backbone.Collection.extend({
+    model: Models.input_descriptor,
+    name: 'input_descriptor',
+    url: API_PREFIX + 'input_descriptor'
+});
+
+collections.input_descriptor = new Collections.input_descriptor();
