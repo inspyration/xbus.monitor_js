776de4ca150be0ceef04603fe0a03c7ba4875dbe
Houzefa Abbasbhay <houzefa.abba@xcg-consulting.fr>
2014-12-15 17:26:39 +0100

    emission_profile -> input_descriptor relation

--- sources/static/js/collections/emission_profile.js
+++ sources/static/js/collections/emission_profile.js
@@ -1,4 +1,9 @@
 Models.emission_profile = Backbone.RelationalModel.extend({
+    relations: [{
+        key: 'input_descriptor_id',
+        relatedModel: 'Models.input_descriptor',
+        type: Backbone.HasOne
+    }],
     urlRoot: API_PREFIX + 'emission_profile'
 });
 
