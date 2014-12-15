fd2348d92af8e48aac934155eea33af20da463b8
Houzefa Abbasbhay <houzefa.abba@xcg-consulting.fr>
2014-12-15 17:05:25 +0100

    Lists: Fetch relational elements before rendering the view for display

--- sources/static/js/views/list.js
+++ sources/static/js/views/list.js
@@ -18,7 +18,7 @@ Views.list = Backbone.View.extend({
         this.rel = options.rel;
         this.template = options.template;
         this.render(); // Pre-render before refreshing anything.
-        this.listenTo(this.collection, 'sync', this.render);
+        this.listenTo(this.collection, 'sync', this.sync);
         this.collection.fetch();
     },
 
@@ -27,11 +27,55 @@ Views.list = Backbone.View.extend({
         this.$el.html(this.template({
             models: this.collection.models,
             name: this.collection.name,
-            rel_name: this.rel ? this.collection.rel_name : this.collection.name,
+            rel_name: this.rel ? this.collection.rel_name
+                : this.collection.name,
             id: this.id,
             rel: this.rel
         }));
         return this;
+    },
+
+    sync: function() {
+        /*
+         * Called when the "sync" event is fired (when data has been fetched
+         * from the server). Before updating the view, also fetch related models
+         * we might need.
+         */
+
+        var that = this;
+
+        // First gather relational fields.
+        var rel_keys = [];
+        _.each(this.collection.model.prototype.relations, function(relation) {
+            rel_keys.push(relation.key);
+        });
+
+        // Render immediately when there are no relational elements.
+        if (_.size(rel_keys) == 0) {
+            this.render();
+            return;
+        }
+
+        // A counter to know when rel models have been fetched.
+        // TODO Improve (with deferreds / promises)...
+        var rel_sync_count = 1;
+
+        // Use the "getAsync" method of backbone-relational to ensure the data
+        // is available.
+        _.each(this.collection.models, function(model) {
+            _.each(rel_keys, function(rel_key) {
+                ++rel_sync_count;
+                model.getAsync(rel_key).done(function() {
+                    if (--rel_sync_count <= 0) {
+                        that.render();
+                    }
+                });
+            });
+        });
+
+        // Wait for 1 run to avoid rendering too often if the data was already
+        // fetched.
+        --rel_sync_count;
     },
 
     addRecord: function(ev) {
